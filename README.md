afcb4afacbf55e8fd847e3e0a81a1624dfd25356
d-smith <doug.smith.mail@gmail.com>
2015-12-18 03:23:50 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# K2ES - Kinesis to Elasticsearch
+
+Use Lambda to move Kinesis data to elastic search
+
+This code was taken from https://github.com/awslabs/amazon-elasticsearch-lambda-samples
+
+## Packaging
+
+To package for upload to amazon lambda, CD into the directory containing
+k2es_lambda.js,then:
+
+<pre>
+npm install aws-sdk
+npm install path
+zip -r k2es.zip *
+</pre>
+
+## Lamba Config
+
+You can now upload the zip when creating the Lambda function. In the Lambda configuration,
+the Handler is k2es_lambda.handler. Create a kinesis execution role, and attach
+a policy allowing access to the elasticsearch stream.
+
+<pre>
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "es:*"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:es:us-east-1:930295567417:domain/xavi1/*"
+        }
+    ]
+}
+</pre>
+
+The inline components of the policy are created in the lambda setup when selecting
+the kinesis execution role.
+
+Note the appropriate Kinesis stream needs to be added as the event source.
+
+## Elasticsearch Config
+
+On the Elasticsearch config side, configure access to the Kibana dashboard
+using the 'Allow access to the domain from specific IP(s)' and specify a CIDR
+spec of allowed address ranges. Alternatively, you could leverage IAM and Cognito
+as well to provide access to specific users or roles.
+
+Beyond access management, the first time Kinana is accessed via the Kibana
+endpoint associated with the domain, use xaviindx* as the index pattern, with
+time as the time field. Note if you use a different index in the
+k2es_lambda.js source, you would use the same value in the index setup.
---
+++ k2es_lambda.js
@@ -0,0 +1,63 @@
+
+var AWS = require('aws-sdk');
+var path = require('path');
+
+/* == Globals == */
+var esDomain = {
+    region: 'us-east-1',
+    endpoint: 'search-xavi1-rpdbu5n5wl2jd3get34cnrqs4q.us-east-1.es.amazonaws.com',
+    index: 'xaviindex',
+    doctype: 'xavilog'
+};
+var endpoint = new AWS.Endpoint(esDomain.endpoint);
+/*
+ * The AWS credentials are picked up from the environment.
+ * They belong to the IAM role assigned to the Lambda function.
+ * Since the ES requests are signed using these credentials,
+ * make sure to apply a policy that allows ES domain operations
+ * to the role.
+ */
+var creds = new AWS.EnvironmentCredentials('AWS');
+
+
+/* Lambda "main": Execution begins here */
+exports.handler = function(event, context) {
+    console.log(JSON.stringify(event, null, '  '));
+    event.Records.forEach(function(record) {
+        var jsonDoc = new Buffer(record.kinesis.data, 'base64');
+        postToES(jsonDoc.toString(), context);
+    });
+}
+
+
+/*
+ * Post the given document to Elasticsearch
+ */
+function postToES(doc, context) {
+    var req = new AWS.HttpRequest(endpoint);
+
+    req.method = 'POST';
+    req.path = path.join('/', esDomain.index, esDomain.doctype);
+    req.region = esDomain.region;
+    req.headers['presigned-expires'] = false;
+    req.headers['Host'] = endpoint.host;
+    req.body = doc;
+
+    var signer = new AWS.Signers.V4(req , 'es');  // es: service code
+    signer.addAuthorization(creds, new Date());
+
+    var send = new AWS.NodeHttpClient();
+    send.handleRequest(req, null, function(httpResp) {
+        var respBody = '';
+        httpResp.on('data', function (chunk) {
+            respBody += chunk;
+        });
+        httpResp.on('end', function (chunk) {
+            console.log('Response: ' + respBody);
+            context.succeed('Lambda added document ' + doc);
+        });
+    }, function(err) {
+        console.log('Error: ' + err);
+        context.fail('Lambda failed with error ' + err);
+    });
+}
